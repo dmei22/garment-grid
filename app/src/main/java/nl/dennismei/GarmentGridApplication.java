70019b2cbe08699cff32e412a8b6b76ab21aaa7e
Dennis <dmei22@hotmail.com>
2026-03-12 19:28:30 +0100

    Added Spring Boot (app: starter, api: starter-web) to project

---
+++ app/src/main/java/nl/dennismei/GarmentGridApplication.java
@@ -0,0 +1,13 @@
+package nl.dennismei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarmentGridApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarmentGridApplication.class, args);
+    }
+
+}
